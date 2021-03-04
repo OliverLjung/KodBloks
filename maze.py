6df12e9b30ff0d12b3d2f555fb5618a0582c182f
waled10 <60223978+waled10@users.noreply.github.com>
2021-03-04 14:35:55 +0100

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,66 +1,77 @@
+
 import pygame
 #import random
 
-win_width = 515
-win_height = 515
-FPS = 60
+class Maze:
+    def window(self):
+        self.win_width = 515
+        self.win_height = 515
+        self.FPS = 60
 
-window = pygame.display.set_mode((win_width, win_height)) # Window
-pygame.display.set_caption("Maze Grid") # Window title
+        self.window_s = pygame.display.set_mode((self.win_width, self.win_height)) # Window
+        pygame.display.set_caption("Maze Grid") # Window title
 
-#rbg
-black = (0, 0, 0)
-white = (255, 255,255)
-red = (255, 0, 0)
+        #rbg
+        self.black = (0, 0, 0)
+        self.white = (255, 255,255)
+        self.red = (255, 0, 0)
 
-width = 46
-height = 46
-margin = 5
-
-# Size of the grid
-grid = []
-for row in range(10):
-    grid.append([])
-    for column in range(10):
-        grid[row].append(0)
+        self.width = 46
+        self.height = 46
+        self.margin = 5
 
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
-    while run:
-        clock.tick(FPS)
+        self.grid = []
+        # Size of the grid
+        for row in range(10):
+            self.grid.append([])
+            for column in range(10):
+                self.grid[row].append(0)
+
+
+    def draw(self):
+        self.run = True
+    
+        self.clock = pygame.time.Clock()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 print("You have quit the window")
             if event.type == pygame.MOUSEBUTTONDOWN:
-                position = pygame.mouse.get_pos()
-                column = position[0] // (width + margin)
-                row = position[1] // (width + margin)
+                self.position = pygame.mouse.get_pos()
+                self.column = self.position [0] // (self.width + self.margin)
+                self.row = self.position [1] // (self.width + self.margin)
 
-                grid[row][column] = 1
+                self.grid[self.row][self.column] = 1
 
-                print("Clicked", position, "grid coordinates: ", row, column)
+                print("Clicked", self.position, "grid coordinates: ", self.row, self.column)
 
-        window.fill(black)
+            self.window_s.fill(self.black)
 
-        # Draw the grid
-        for row in range(10):
-            for column in range(10):
-                cell_color = white
-                if grid[row][column] == 1:
-                    cell_color = red
-                pygame.draw.rect(window, cell_color,
-                                [(margin + width) * column + margin,
-                                (margin + height) * row + margin,
-                                width,
-                                height]) 
-        
-        clock.tick(FPS)
-        pygame.display.flip()
+            # Draw the grid
+            for row in range(10):
+                for column in range(10):
+                    self.cell_color = self.white
+                    if self.grid[row][column] == 1:
+                        self.cell_color = self.red
+                    pygame.draw.rect(self.window_s, self.cell_color,
+                                    [(self.margin + self.width) * column + self.margin,
+                                    (self.margin + self.height) * row + self.margin,
+                                    self.width,
+                                    self.height]) 
+            
+            self.clock.tick(self.FPS)
+            pygame.display.flip()
 
 
 
-main()
+
+game = Maze()
+
+game.window()
+
+while True:
+    game.draw()
+
+
 pygame.quit()
