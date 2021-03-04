90212f205d353ad5394cc13771efa4800e533035
OliverLjung <64260361+OliverLjung@users.noreply.github.com>
2021-03-04 13:57:01 +0100

    Added geters and size for map
    To acceses map and its size, you use the Game.map and Game.mapSize geter functions.

--- game.py
+++ game.py
@@ -5,12 +5,13 @@ class Game:
 
     def __init__(self):
         "Initilizes a Game"
-
-        self._map = []
+        self._character = Character()
+        self.start()
 
     def start(self):
         "Starts a Game"
-
+        self._map = []
+        self._mapSize = (0, 0)
         self.getMap()
         self.drawGame()
 
@@ -25,14 +26,33 @@ class Game:
         randomMapInt = random.randint(1, 1)
         _file = open(f"maps/map{randomMapInt}.txt", "r")
         _lines = _file.readlines()
+        _width = 0
+        _height = 0
         for line in _lines:
+            _height +=1
             lineList = []
             for bit in line:
                 if bit.isnumeric():
+                    _width +=1
                     lineList.append(int(bit))
             self._map.append(lineList)
 
+        self._mapSize = (_width, _height)
         _file.close()
+
+    @property
+    def map(self):
+        return self._map
+
+    @property
+    def mapSize(self):
+        return self._mapSize
+
+
+class Character():
+    def __init__(self):
+        pass
+
 
 # TEST
 mygame = Game()
